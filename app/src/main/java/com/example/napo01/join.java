efc89d3d3774d92be310db4ef684bc2f09c6266e
so9022 <so9022@naver.com>
2022-08-30 09:18:29 +0900

    회원가입 서버추가

--- app/src/main/java/com/example/napo01/join.java
+++ app/src/main/java/com/example/napo01/join.java
@@ -1,14 +1,103 @@
 package com.example.napo01;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class join extends AppCompatActivity {
+    private EditText edt_id, edt_mpw, edt_name, edt_phone;
+    private Button btn_mjoin;
+
+    private RequestQueue queue;
+    private StringRequest stringRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
+
+        edt_id = findViewById(R.id.edt_id);
+        edt_mpw = findViewById(R.id.edt_mpw);
+        edt_name = findViewById(R.id.edt_name);
+        edt_phone = findViewById(R.id.edt_phone);
+        btn_mjoin = findViewById(R.id.btn_mjoin);
+        btn_mjoin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendRequest();
+            }
+        });
+    }
+
+    public void sendRequest(){
+        // Volley Lib 새로운 요청객체 생성
+        queue = Volley.newRequestQueue(this);
+        // 서버에 요청할 주소
+        String url = "http://192.168.21.66:5001/join";
+        // 요청 문자열 저장
+        stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            // 응답데이터를 받아오는 곳
+            @Override
+            public void onResponse(String response) {
+                Log.v("resultValue",response);
+            }
+        }, new Response.ErrorListener() {
+            // 서버와의 연동 에러시 출력
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        }){
+            @Override //response를 UTF8로 변경해주는 소스코드
+            protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                try {
+                    String utf8String = new String(response.data, "UTF-8");
+                    return Response.success(utf8String, HttpHeaderParser.parseCacheHeaders(response));
+                } catch (UnsupportedEncodingException e) {
+                    // log error
+                    return Response.error(new ParseError(e));
+                } catch (Exception e) {
+                    // log error
+                    return Response.error(new ParseError(e));
+                }
+            }
+            // 보낼 데이터를 저장하는 곳
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                String id =edt_id.getText().toString();
+                String pw = edt_mpw.getText().toString();
+                String name = edt_name.getText().toString();
+                String phone = edt_phone.getText().toString();
+                params.put("id",id);
+                params.put("pw",pw);
+                params.put("name",name);
+                params.put("phone",phone);
+
+                return params;
+            }
+        };
+        String TAG = "chanho";
+        stringRequest.setTag(TAG);
+        queue.add(stringRequest);
     }
 }
